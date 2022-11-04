1db18a1d74a8aeadd6fa3e7950aa5fd21d6131cf
Damian Kucharski <damiankuchar1@gmail.com>
2022-11-04 22:26:11 +0100

    Added registration form

--- frontend/rental/src/routes/register.tsx
+++ frontend/rental/src/routes/register.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { Box, Button, Divider, TextField, Typography } from '@mui/material'
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <form noValidate>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        maxWidth={300}
+        margin="auto"
+        marginTop={2}
+        padding={3}
+      >
+        <Typography variant="h3" mb={2}>
+          Sign up
+        </Typography>
+        <Divider sx={{ width: "100%" }} />
+        <TextField type="email" label="Email" margin="normal" autoFocus />
+        <TextField
+          type="password"
+          label="Password"
+          margin="normal"
+          style={{ marginBottom: "4px" }}
+        />
+        <TextField
+          type="password"
+          label="Confirm password"
+          margin="normal"
+          style={{ marginBottom: "4px" }}
+        />
+        <Box>
+          <Button
+            variant="text"
+            component={Link}
+            to="/login"
+            style={{ padding: 0, fontSize: 9, marginLeft: "145px" }}
+          >
+            Sign in <ArrowForwardIcon style={{ fontSize: 10 }} />
+          </Button>
+        </Box>
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ marginTop: 2, bgcolor: "#DD5353" }}
+        >
+          <Typography>Sign up</Typography>
+        </Button>
+      </Box>
+    </form>
   )
 }
 

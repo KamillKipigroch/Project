f53648424742dffdd325c09f61d71fbb4048562b
Damian Kucharski <damiankuchar1@gmail.com>
2022-11-04 20:01:41 +0100

    Added login form

--- frontend/rental/src/routes/login.tsx
+++ frontend/rental/src/routes/login.tsx
@@ -1,9 +1,50 @@
-import Image from "../assets/pexels-photo-1308648.jpeg";
+import { Box, Button, Divider, TextField, Typography } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Link } from "react-router-dom";
+import { bgcolor } from "@mui/system";
 
 export default function Home() {
-    return (
-      <div>
-        Login
-      </div>
-    );
-  }
+  return (
+    <form noValidate>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        maxWidth={300}
+        margin="auto"
+        marginTop={2}
+        padding={3}
+      >
+        <Typography variant="h3" mb={2}>
+          Logowanie
+        </Typography>
+        <Divider sx={{ width: "100%" }} />
+        <TextField type="email" label="Email" margin="normal" autoFocus />
+        <TextField
+          type="password"
+          label="Hasło"
+          margin="normal"
+          style={{ marginBottom: "4px" }}
+        />
+        <Box>
+          <Button
+            variant="text"
+            component={Link}
+            to="/register"
+            style={{ padding: 0, fontSize: 9, marginLeft: "115px" }}
+          >
+            Zarejestruj się <ArrowForwardIcon style={{ fontSize: 10 }} />
+          </Button>
+        </Box>
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ marginTop: 2, bgcolor: "#DD5353" }}
+        >
+          <Typography>Zaloguj się</Typography>
+        </Button>
+      </Box>
+    </form>
+  );
+}

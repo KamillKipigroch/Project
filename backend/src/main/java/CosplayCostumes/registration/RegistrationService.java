5fa190c2cca3160ce18a70de881fe2e20b5ed99a
Kamil Kipigroch <kamil_kipigroch@o2.pl>
2023-01-07 14:48:39 +0100

    update some application properties and change url database to aws

--- backend/src/main/java/CosplayCostumes/registration/RegistrationService.java
+++ backend/src/main/java/CosplayCostumes/registration/RegistrationService.java
@@ -4,7 +4,6 @@ package CosplayCostumes.registration;
 import CosplayCostumes.registration.token.ConfirmationToken;
 import CosplayCostumes.registration.token.ConfirmationTokenService;
 import CosplayCostumes.rest.model.dto.RegisterUserRequest;
-import CosplayCostumes.security.sender.EmailSender;
 import CosplayCostumes.security.user.model.LoginUser;
 import CosplayCostumes.security.user.model.User;
 import CosplayCostumes.security.user.model.UserRole;
@@ -27,7 +26,6 @@ public class RegistrationService {
     private final ConfirmationTokenService confirmationTokenService;
 
     private UserService userService;
-    private final EmailSender emailSender;
 
 
     public String register(RegistrationRequest request) {
@@ -46,14 +44,6 @@ public class RegistrationService {
                 );
 
         String link = "http://localhost:5000/api/v1/registration/confirm?token=" + token;
-        try{
-            emailSender.send(
-                    request.getEmail(),
-                    buildEmail(request.getFirstName(), link));
-        }
-        catch (Exception e) {
-            logger.error("Email sender dont work !");
-        }
         return token;
     }
 
@@ -78,42 +68,6 @@ public class RegistrationService {
         userService.enableUser(
                 confirmationToken.getUser().getEmail());
         return "confirmed";
-    }
-
-    private String buildEmail(String name, String link) {
-        return "<div>\n" +
-                "  <table role=\"presentation\" style=\"min-width:100%;!important\">\n" +
-                "    <tr>\n" +
-                "      <td>\n" +
-                "        <table role=\"presentation\" width=\"100%\" style=\"max-width:480pt\" align=\"center\">\n" +
-                "          <tr>\n" +
-                "            <td style=\"background-color:#1D70B8;font-size:28px;padding-left:30pt\">\n" +
-                "                    <span style=\"color:#ffffff\">Confirm your email</span>\n" +
-                "            </td>\n" +
-                "          </tr>\n" +
-                "        </table>\n" +
-                "      </td>\n" +
-                "    </tr>\n" +
-                "  </table>\n" +
-                "  <table role=\"presentation\" align=\"center\"\n" +
-                "         style=\"border-collapse:collapse;max-width:400pt;width:100%!important\">\n" +
-                "    <tbody>\n" +
-                "    <tr>\n" +
-                "      <td style=\"padding-top:15pt;font-size:19px\">\n" +
-                "        Hi " + name + "\n" +
-                "        <br/> Thank you for registering in CosplayCostumes.\n" +
-                "        <br/> Please click on the link to activate your account:\n" +
-                "        <p></p>\n" +
-                "        <blockquote\n" +
-                "          style=\"background-color: #f2f1ef;padding:12pt;font-size:19px\">\n" +
-                "          <a href=\""+ link +"\">Activate your account in CosplayCostumes !</a>\n" +
-                "        </blockquote>\n" +
-                "        Link will expire in 15 minutes. <p>See you soon</p>\n" +
-                "      </td>\n" +
-                "    </tr>\n" +
-                "    </tbody>\n" +
-                "  </table>\n" +
-                "</div>\n";
     }
 
     public User registerUser(RegisterUserRequest request) {
--- backend/src/main/java/CosplayCostumes/security/sender/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package CosplayCostumes.security.sender;
-
-public interface EmailSender {
-    void send(String to, String email);
-}

33988e7500d300166854cd487be7d215d5e3009b
Kamil Kipigroch <kamil_kipigroch@o2.pl>
2023-01-09 10:31:05 +0100

    change slider for price color on change website mode

--- frontend/rental/src/components/rangeSlider.tsx
+++ frontend/rental/src/components/rangeSlider.tsx
@@ -1,40 +1,47 @@
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { observer } from 'mobx-react-lite';
-import { useStores } from '../stores/root.store';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {observer} from 'mobx-react-lite';
+import {useStores} from '../stores/root.store';
+import {useTheme} from "@mui/material";
+import * as React from "react";
 
-const theme = createTheme({
+const mainTheme = createTheme({
     palette: {
-      secondary: {
-        main: '#333333'
-      }
+        secondary: {
+            main: '#333333'
+        }
     }
 });
 
 function valuetext(value: number) {
-  return `${value}°C`;
+    return `${value}°C`;
 }
 
 const RangeSlider = () => {
-  const { productStore } = useStores();
+    const {productStore} = useStores();
+    const theme = useTheme();
 
-  return (
-    <Box sx={{ width: 200 }}>
-        <ThemeProvider theme={theme}>
-            <Slider
-                min={productStore.priceMin}
-                max={productStore.maxPriceValue}
-                getAriaLabel={() => 'Temperature range'}
-                value={productStore.priceValue}
-                onChange={productStore.handlePriceRangeFilterChange}
-                valueLabelDisplay="auto"
-                getAriaValueText={valuetext}
-                color="secondary"
-            />
-        </ThemeProvider>
-    </Box>
-  );
+    return (
+        <Box sx={{width: 200}}>
+            <ThemeProvider theme={mainTheme}>
+                <Slider
+                    min={productStore.priceMin}
+                    max={productStore.maxPriceValue}
+                    getAriaLabel={() => 'Temperature range'}
+                    value={productStore.priceValue}
+                    onChange={productStore.handlePriceRangeFilterChange}
+                    valueLabelDisplay="auto"
+                    getAriaValueText={valuetext}
+                    color={theme.palette.mode === "light" ? (
+                        "secondary"
+                    ) : (
+                        "primary"
+                    )}
+                />
+            </ThemeProvider>
+        </Box>
+    );
 }
 
 export default observer(RangeSlider);
